a555e0d48fe0fb977a400a4c52e0b245814a2b28
Alberto de Blas <alberto.de.blas@gmail.com>
2020-06-16 21:45:29 +0200

    Add TestCase
    feat: Add TestCase

---
+++ TestCase.py
@@ -0,0 +1,4 @@
+class TestCase:
+
+    def __init__(self, name):
+        self.name = name
--- WasRun.py
+++ WasRun.py
@@ -1,16 +1,20 @@
-class WasRun:
+from TestCase import TestCase
+
+
+class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
-        self.name = name
-
+        TestCase.__init__(self, name)
 
     def run(self):
         method = getattr(self, self.name)
         method()
 
+    def test_method(self):
+        pass
 
 
-test = WasRun("testMethod")
+test = WasRun("test_method")
 print(test.wasRun)
 test.run()
 print(test.wasRun)
